e3ce7841bcb2dbc0abdc8622dc5c825fd10603a0
Lem <enming.cityu@gmail.com>
2026-06-09 14:15:49 +0800

    update

--- src/homopt/viz/__init__.py
+++ src/homopt/viz/__init__.py
@@ -1,9 +1,7 @@
-"""Visualization namespace."""
+"""Hom-PGD visualization namespace."""
 
-from ._coninn_impl import evaluate_coninn_exactness, visualize_coninn_exactness
 from ._convex2d_impl import save_convex_2d_visualizations
 from .comparison import save_comparison_visualizations
-from .inn_training import save_inn_training_visualizations
 from .style import (
     ALGORITHM_COLORS,
     ALGORITHM_LINE_STYLES,
@@ -16,7 +14,6 @@ from .traces import (
     build_convex_2d_traces,
     split_convex_metrics,
 )
-from ._mdh_impl import visualize_mdh_mapping_transformation
 
 __all__ = [
     "ALGORITHM_COLORS",
@@ -26,11 +23,7 @@ __all__ = [
     "apply_paper_style_rcparams",
     "build_convex_2d_traces",
     "configure_matplotlib_cache",
-    "evaluate_coninn_exactness",
     "save_convex_2d_visualizations",
     "save_comparison_visualizations",
-    "save_inn_training_visualizations",
     "split_convex_metrics",
-    "visualize_coninn_exactness",
-    "visualize_mdh_mapping_transformation",
 ]
